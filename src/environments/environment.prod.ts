47852d298a8322b0549a6d12d5a320fcafdc546a
Eric Lebetsamer <elebet@gmail.com>
2017-09-15 23:06:18 -0700

    Add other config data to environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  analytics: {
+    trackingId: 'UA-255878-12'
+  },
+  firebase: {
+    apiKey: 'AIzaSyDIXAqHr-gewCBtcl4ZssqplBzvj3ywv40',
+    authDomain: 'math-worksheet-generator.firebaseapp.com',
+    databaseURL: 'https://math-worksheet-generator.firebaseio.com',
+    projectId: 'math-worksheet-generator',
+    storageBucket: 'math-worksheet-generator.appspot.com',
+    messagingSenderId: '641444124957'
+  }
 };
