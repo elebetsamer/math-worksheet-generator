e82dff92c788ddd9354f86174b833a0cb2446fad
Eric Lebetsamer <elebet@gmail.com>
2017-09-15 22:42:28 -0700

    Add firebase modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AnalyticsService } from './analytics/analytics.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthService } from './auth/auth.service';
+import { environment } from '../environments/environment';
 import { WorksheetGeneratorComponent } from './worksheet-generator/worksheet-generator.component';
 import { WorksheetService } from './worksheet/worksheet.service';
 import { MaterialComponentModule } from './material/material-component.module';
@@ -22,7 +27,10 @@ import { SafePipe } from './safe.pipe';
     FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialComponentModule
+    MaterialComponentModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   providers: [
     AnalyticsService,
