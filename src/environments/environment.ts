72ba133dc5704daef091280fcb8834a88ce77e11
Eric Lebetsamer <elebet@gmail.com>
2017-09-15 22:41:57 -0700

    Add firebase api config data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,13 @@ export const environment = {
   production: false,
   analytics: {
     trackingId: 'UA-255878-12'
+  },
+  firebase: {
+    apiKey: 'AIzaSyDIXAqHr-gewCBtcl4ZssqplBzvj3ywv40',
+    authDomain: 'math-worksheet-generator.firebaseapp.com',
+    databaseURL: 'https://math-worksheet-generator.firebaseio.com',
+    projectId: 'math-worksheet-generator',
+    storageBucket: 'math-worksheet-generator.appspot.com',
+    messagingSenderId: '641444124957'
   }
 };
